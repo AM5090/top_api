7d7bf9dab293531e9967f668bdeb30db9a1a8c11
AM <armen.melkonyan.93@inbox.ru>
2025-08-13 18:31:37 +0300

    Подключение к бд по новым урокам

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserModel, UserModelDocument } from './models/user.model';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(UserModel.name) private userModel: Model<UserModelDocument>,
+  ) {}
+
+  async getByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  async createUser({ email, password }: { email: string; password: string }) {
+    const newUser = new this.userModel({ email, password });
+    return newUser.save();
+  }
+}
