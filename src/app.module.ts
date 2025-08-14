26525f697f89b7892d1780b40d401f60536fb052
AM <armen.melkonyan.93@inbox.ru>
2025-08-14 17:41:16 +0300

    Подключил схемы к бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { getMongoConfig } from './config/mongo.config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // MongooseModule.forRoot('mongodb://localhost:27017/top_api'),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,56 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disAdvantages: string;
-  categories: string[];
-  tags: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+class ProductCharacteristic {
+  @Prop()
+  name: string;
+
+  @Prop()
+  value: string;
 }
+
+export const ProductCharacteristicSchema = SchemaFactory.createForClass(
+  ProductCharacteristic,
+);
+
+@Schema({ timestamps: true })
+export class ProductModel {
+  @Prop()
+  image: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  oldPrice: number;
+
+  @Prop()
+  credit: number;
+
+  @Prop()
+  calculatedRating: number;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  advantages: string;
+
+  @Prop()
+  disAdvantages: string;
+
+  @Prop([String])
+  categories: string[];
+
+  @Prop([String])
+  tags: string[];
+
+  @Prop({ type: [ProductCharacteristicSchema], _id: false })
+  characteristics: ProductCharacteristic[];
+}
+
+export type ProductModelDocument = HydratedDocument<ProductModel>;
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
